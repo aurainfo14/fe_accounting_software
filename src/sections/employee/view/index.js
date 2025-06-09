6789ac36f09dcbcd6ab9b4a134a60b03f6668714
KaushalSojitra1911 <sujalpaghadal17@gmail.com>
2025-06-09 18:08:08 +0530

    commit

--- src/sections/employee/view/index.js
+++
@@ -1,5 +0,0 @@
-export { default as UserEditView } from './employee-edit-view';
-// export { default as UserListView } from './inquiry-list-view';
-// export { default as UserCardsView } from './employee-cards-view';
-// export { default as UserCreateView } from './employee-create-view';
-export { default as UserProfileView } from './user-profile-view';
